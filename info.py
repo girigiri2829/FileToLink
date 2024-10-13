085a5092478e5eb79244cd959f1a5e95f723b84a
Sridharan <128954293+girigiri2829@users.noreply.github.com>
2024-10-13 16:56:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ MULTI_CLIENT = True
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
-    ON_HEROKU = True
+    ON_HEROKU = False
 else:
     ON_HEROKU = True
 URL = environ.get("URL", "")
